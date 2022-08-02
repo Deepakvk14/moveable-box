c53b79704ba989e1c5618650d4fce12eba3a42b0
Deepak Verma <82318692+Deepakvk14@users.noreply.github.com>
2022-08-02 12:12:53 +0530

    Add files via upload
    "first - commit"

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Controller from './components/Controller/Controller'
+import Screen from './components/Screen/Screen'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <Controller/>
+      <Screen/>
+    </>
+  )
+}
+
+export default App
